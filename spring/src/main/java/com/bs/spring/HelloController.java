36e0920f8f3cd1affd5ee88669a5cba1400a3892
simjiho <simjiho4584@gmail.com>
2024-07-06 17:31:26 +0900

    2024-07-06 스프링 수업

---
+++ spring/src/main/java/com/bs/spring/HelloController.java
@@ -0,0 +1,19 @@
+package com.bs.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	public HelloController() {
+		System.out.println("HelloController생성 :)");
+	}
+	
+	@RequestMapping("/")
+	public String mainpage() {
+		System.out.println("mainpage메소드 실행!!");
+		return "index";
+	}
+	
+}
---
+++ spring/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>나의 첫 스프링페이지</title>
+</head>
+<body>
+	<h2>우와 나의 메인페이지!!</h2>
+</body>
+</html>
